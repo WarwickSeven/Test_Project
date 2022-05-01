eecd1173b7c7da051e55108d1a7be63e6b5fba35
WarwickSeven <WarwickSeven@gmail.com>
2022-05-01 23:14:03 +0300

    Try 1

--- Test_Project/main.cpp
+++ Test_Project/main.cpp
@@ -1,14 +1,8 @@
-//
-//  main.cpp
-//  Test_Project
-//
-//  Created by Александр Тихонов on 01.05.2022.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
+int main() {
+    int var = 0;
+    std::cout << "Your var is: " << var << std::endl;
     std::cout << "Hello, World!\n";
     return 0;
 }
